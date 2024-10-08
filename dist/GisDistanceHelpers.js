d8ad601f624f996ce246ba81c2a9e7ba03f7d169
Khamphet LUONGPHAYBOUN <khamphet046@gmail.com>
2024-10-08 13:25:39 +0700

    feat: isPointInPolygon method

--- dist/GisDistanceHelpers.js
+++ dist/GisDistanceHelpers.js
@@ -28,5 +28,26 @@ class GisDistanceHelpers {
     getLonLat2() {
         return [this.location2[1], this.location2[0]];
     }
+    isPointInPolygon(latitude, longitude, polygon) {
+        if (typeof latitude !== 'number' || typeof longitude !== 'number') {
+            throw new TypeError('Invalid latitude or longitude. Numbers are expected');
+        }
+        else if (!Array.isArray(polygon) || polygon.length === 0) {
+            throw new TypeError('Invalid polygon. Non-empty array with locations expected');
+        }
+        const x = latitude;
+        const y = longitude;
+        let inside = false;
+        for (let i = 0, j = polygon.length - 1; i < polygon.length; j = i++) {
+            const xi = polygon[i][0];
+            const yi = polygon[i][1];
+            const xj = polygon[j][0];
+            const yj = polygon[j][1];
+            const intersect = (yi > y) !== (yj > y) && x < ((xj - xi) * (y - yi)) / (yj - yi) + xi;
+            if (intersect)
+                inside = !inside;
+        }
+        return inside;
+    }
 }
 exports.GisDistanceHelpers = GisDistanceHelpers;
